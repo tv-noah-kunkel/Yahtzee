e1478e9c2a1189aef2570b7d6f4f4d93526ef8d7
tv-noah-kunkel <noahpeely@gmail.com>
2025-12-10 10:38:32 -0700

    graham coleslaw

--- Player.java
+++ Player.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Player {
     private  ArrayList<Die> hand = new ArrayList<>();
@@ -11,6 +12,7 @@ public class Player {
     }
     DicePrinter DicePrinter = new DicePrinter();
     public void playTurn(){
+    Scanner scanner = new Scanner(System.in);
     for(int i = 0; i < 5; i++)
     {
         Die die = new Die();
@@ -18,7 +20,40 @@ public class Player {
         
         hand.add(die);
     }
+   System.out.println("which dice would you like to hold (put 0 if none)");
+   String holding = scanner.nextLine();
+   if(holding.contains("5"))
+   {
+    heldDice.add(hand.get(4));
+    hand.remove(4);
+   }
+   if(holding.contains("4"))
+   {
+    heldDice.add(hand.get(3));
+    hand.remove(3);
+   }
+    
+   if(holding.contains("3"))
+   {
+    heldDice.add(hand.get(2));
+    hand.remove(2);
+   }
+    
+   if(holding.contains("2"))
+   {
+    heldDice.add(hand.get(1));
+    hand.remove(1);
+   }
+    
+   if(holding.contains("1"))
+   {
+    heldDice.add(hand.get(0));
+    hand.remove(0);
+   }
+
     DicePrinter.print(hand);
+    
+    
     }
                                                                
 }
